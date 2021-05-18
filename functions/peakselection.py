61fa0fd3fb8efdb977f63d120934c78cc2124a71
Antonio Casoli <voxeldoodle@gmail.com>
2021-05-18 15:06:39 +0300

    First peak selection method

--- functions/peakselection.py
+++ functions/peakselection.py
@@ -50,3 +50,55 @@ def selectPeaks(peaklist, n, k, select=selectPeaksMinimumError):
 		test = test.append(ts)
 		train = train.append(tr)
 	return train, test
+
+'''
+This function selects a set amount n of the best peaks to lead the calibration process, where best means the minum error (abs(detected_mz-observed_mz)).
+It is possible to set a treshold to specifiy the minimal value of error accepted.
+The threshold criteria is secondary to the number of peaks, meaning that if the threshold isn't met and the number of peaks selected is under n, it will put those below the threshold to reach n. To change this set the forceThreshold parameter to True.
+To disable the threshold set it to 0 (default value).
+The treshold is treated as absolute value, so negative values don't matter.
+This method provides the remaining peaks as a test set, and the size can be specified with the test_n parameter. If left at 0 it will return all the discarded data for testing.
+The randomTestSamples parameter allows to sample randomly from the remaining data, otherwise they're provided ordered by least uncerteinty value.
+'''
+def selectBestPeaks(data, n=10, threshold=0, forceThreshold=False, test_n=0, randomTestSamples=True, debug=False):
+	if n <=0:
+		return None
+	result = data.copy()
+	result["absolute_error"] =  abs(data["error"])
+	result = result.sort_values(by=['absolute_error'], ascending=True) #specified ascending parameter for flexibility
+	diff = result
+	test_n = abs(test_n)
+
+	if debug:
+		print(result)
+
+	if threshold:
+		threshold = abs(threshold)
+		filtered = result[result['absolute_error']<=threshold]
+
+	if len(result) > n:
+		if threshold:
+			if len(filtered) > n:
+				result = filtered[0:n]
+			else:
+				if forceThreshold:
+					result = filtered
+				else:
+					result = result[0:n]
+		else:
+			result = result[0:n]
+	else:
+		if forceThreshold and threshold:
+			result = filtered
+		else:
+			x = max(len(result), n)
+			result = result[0:x]
+
+	diff = pd.concat([result,diff]).drop_duplicates(keep=False)
+	if test_n != 0:
+		test_n = min(test_n, len(diff))
+		if randomTestSamples:
+			diff = diff.sample(test_n)
+		else:
+			diff = diff[0:test_n]
+	return result, diff
